d2a4dd6bcded5e9e4cb86a13bbc0f9abd050ab23
Ralph Seaman <ralph.seaman@mlb.com>
2022-08-24 13:37:03 -0400

    spm init artifacts

---
+++ Sources/AEPProtocols/AEPProtocols.swift
@@ -0,0 +1,6 @@
+public struct AEPProtocols {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/AEPProtocolsTests/AEPProtocolsTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import AEPProtocols
+
+final class AEPProtocolsTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(AEPProtocols().text, "Hello, World!")
+    }
+}
